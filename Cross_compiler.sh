3b56a1b4cbd0912ac8c1f928353aa68972d1337f
Sankalp Ghatpande <deadlyelder@users.noreply.github.com>
2017-06-19 16:17:18 +0200

    Debug, Readme

--- Cross_compiler.sh
+++ Cross_compiler.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 #Normal OS update, upgrade and installing the libraries
 sudo apt-get update
 sudo apt-get upgrade
@@ -10,32 +12,33 @@ mkdir $HOME/src
 cd $HOME/src
 
 # Downloading the Binutilies
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz
-tar xvf binutils-2.24.tar.gz
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
+tar xvf binutils-2.28.tar.gz
 
-'''As mentioned by vaious people, it is dangerous to install a compiler within the system directory.
-So as a workaround we put this in $HOME/opt/cross_compile.
-To have the cross-compiler accessed globally it is ideal to place it at /usr/local/cross'''
+#Downloading gcc
+wget http://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.gz
+# Alternative link
+# wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.gnu.org/gcc/gcc-7.1.0/gcc-7.1.0.tar.gz
+tar xvf gcc-7.1.0.tar.gz
 
-# Creating PATH inside the ~/.bashrc file
-echo " export PREFIX="$HOME/opt/cross_compile" \n export TARGET=x86_64-elf \n export PATH="$PREFIX/bin:$PATH" " >> ~/.bachrc
+# Creating PATH 
+export PREFIX="$HOME/opt/cross"
+export TARGET=x86_64-elf
+export PATH="$PREFIX/bin:$PATH"
 
 #Building the binutilies
-# To be tested: cd $HOME/src
+cd $HOME/src
 mkdir build-binutils
 cd build-binutils
-../binutils-2.14/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror
+../binutils-2.28/configure --target=$TARGET --prefix=/opt/cross --disable-nls --disable-werror
 make -j4
 sudo make -j4 install
 
-#Downloading gcc
-wget http://ftp.gnu.org/gnu/gcc/gcc-4.9.1/gcc-4.9.1.tar.gz
-tar xvf gcc-4.9.1.tar.gz
-
+cd $HOME/src
 #Building the gcc
 mkdir build-gcc
 cd build-gcc
-../gcc-4.9.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+../gcc-7.1.0/configure --target=$TARGET --prefix=/opt/cross --disable-nls --enable-languages=c,c++ --without-headers --disable-multilib
 make -j4 all-gcc
 make -j4 all-target-libgcc
 sudo make install-gcc
---
+++ README.md
@@ -0,0 +1,31 @@
+# cross-compile
+
+cross-compile is my clumsy attempt at creating a cross-compiler for a Debian based system
+from start. The script is a trail attempt and is likely to cause breakdown of a working machine, as such
+its highly recommended that you execute the script within an isolated environment.
+
+The plan is to use this cross-compiler as base for planned future projects.
+
+## How it works
+
+Building a cross-compiler is relatively easy task, although as was realized later on that if its not done with care, it can led to lots of wasted time.
+The script in the repo is pretty much self explanatory and has been successfully tested on the following systems:
+
+* Ubuntu 14.04
+* Ubuntu 16.04
+
+## Credits
+
+Credits to the blogpost and wiki referred in the later section.
+
+## License
+
+[![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)](https://creativecommons.org/publicdomain/zero/1.0/)
+
+This list is released into the public domain.
+
+
+## References
+
+1. [How to build a gcc cross compiler](http://preshing.com/20141119/how-to-build-a-gcc-cross-compiler/) Preshing of Programming Blog post
+2. [GCC Cross-Compiler](http://wiki.osdev.org/GCC_Cross-Compiler) OSDev.org
